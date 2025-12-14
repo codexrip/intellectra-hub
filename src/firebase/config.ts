852e037b9398765f1046f0129412c509f68f4ed1
Muhammad Ammar Hamza <241124@students.au.edu.pk>
2025-12-14 07:45:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8938028267-ae955",
+  "appId": "1:925677887374:web:70490424310c7c8632cedf",
+  "apiKey": "AIzaSyBT9xxuNZr0deXM2pH8LnV2Eihp68LhoAE",
+  "authDomain": "studio-8938028267-ae955.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "925677887374"
+};
